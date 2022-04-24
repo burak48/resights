a8dcb8d438f66303934179bb6f48943c4f19ea71
burak48 <buraksaglam3@gmail.com>
2022-04-24 19:30:21 +0300

    sort column by gender && search records

--- components/DataTable.vue
+++ components/DataTable.vue
@@ -1,24 +1,34 @@
 <template lang="pug">
-  v-data-table(
-    :headers="headers"
-    :items="items"
-    item-key="email"
-    dense
-  ).elevation-1.mt-10
+  div
+    v-text-field(
+      v-model="search"
+      append-icon="mdi-magnify"
+      label="Search"
+      single-line
+      hide-details
+    )
+    v-data-table(
+      :headers="headers"
+      :items="items"
+      item-key="email"
+      dense
+      sort-by="gender"
+      :search="search"
+    ).elevation-1.mt-10
 </template>
 
 <script>
 export default {
-  props: ['headers', 'items'],
+  props: ["headers", "items"],
   data() {
     return {
-      
-    }
+      search: ""
+    };
   }
-}
+};
 </script>
 
 <style lang="sass" scoped>
-  .v-data-table
-    max-width: 100%
+.v-data-table
+  max-width: 100%
 </style>
